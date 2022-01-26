c96989493a157d2a87e5d4b99f25b3a6e23ca05e
Shreya <dhirshreya@gmail.com>
2022-01-26 17:28:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I’m <b>Shreya</b>. I’m a sophomore at <b>KIIT, Bhubaneshwar</b> and a <b>ReactJS</b> and <b>React Native</b> developer. You can contact me on <a href="https://twitter.com/ShreyaDhir">Twitter</a>, <a href="https://github.com/ShreyaDhir">Github</a> and <a href="https://www.linkedin.com/in/shreya-dhir/">LinkedIn</a>.</p>
+        <p>Hello, I’m <b>Shreya</b>. I’m a junior at <b>KIIT, Bhubaneshwar</b> and a <b>ReactJS</b> and <b>React Native</b> developer. You can contact me on <a href="https://twitter.com/ShreyaDhir">Twitter</a>, <a href="https://github.com/ShreyaDhir">Github</a> and <a href="https://www.linkedin.com/in/shreya-dhir/">LinkedIn</a>.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
